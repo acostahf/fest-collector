c473e52b5c156c5310689c09ef71b25a49056bbd
Fabian Acosta <acosahf@hotmail.com>
2020-01-01 15:34:41 -0600

    updated the festival_index view and created festivals_details

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Festival
 
 
-class Festival:  # Note that parens are optional if not inheriting from another class
-    def __init__(self, name, location, description, year):
-        self.name = name
-        self.location = location
-        self.description = description
-        self.year = year
+# class Festival:  # Note that parens are optional if not inheriting from another class
+#     def __init__(self, name, location, description, year):
+#         self.name = name
+#         self.location = location
+#         self.description = description
+#         self.year = year
 
 
 festivals = [
-    Festival('Lolo', 'tabby', 'foul little demon', 3),
-    Festival('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Festival('Raven', 'black tripod', '3 legged cat', 4)
+    Festival('ACL', 'Austin', '3day two weekend', 2019),
+    Festival('EDC', 'Las Vegas', 'Ravers everywhere', 2018),
+    Festival('Electric Forest', 'Michigan',
+             'the forest becomes a huge rave', 2016)
 ]
 
 # Create your views here.
@@ -28,4 +30,9 @@ def about(request):
 
 
 def festivals_index(request):
+    festivals = Festival.objects.all()
     return render(request, 'festivals/index.html', {'festivals': festivals})
+
+def festivals_detail(request, festival_id):
+    festival = Festival.objects.get(id=festival_id)
+    return render(request, 'festivals/detail.html', { 'festival': festival })
