5c5b27d7d408020cb3cc4d64d29ee3dd59851ea2
Fabian Acosta <acosahf@hotmail.com>
2020-01-01 15:33:36 -0600

    created the festival and rating models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+VIBES = (
+    ('B', 'Bad'),
+    ('G', 'Good'),
+    ('P', 'Plur')
+)
+
+
+class Festival(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    year = models.IntegerField()
+
+
+class Rating(models.Model):
+    date = models.DateField()
+    vibe = models.CharField(
+        max_length=1,
+        choices=VIBES,
+        default=VIBES[0][0]
+    )
