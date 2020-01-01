80d71805a526a4360dd402579ba105db3958bbec
Fabian Acosta <acosahf@hotmail.com>
2020-01-01 16:19:05 -0600

    add get absollute url to the festival model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 VIBES = (
@@ -13,6 +14,14 @@ class Festival(models.Model):
     location = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     year = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
+    
+  # Add this method
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'festival_id': self.id})
 
 
 class Rating(models.Model):
