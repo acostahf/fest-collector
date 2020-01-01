3899b590c817173b1f6a6f24ade1c6fbcec62b40
Fabian Acosta <acosahf@hotmail.com>
2020-01-01 16:20:49 -0600

    add classbased views paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('festivals/', views.festivals_index, name='index'),
     path('festivals/<int:festival_id>/', views.festivals_detail, name='detail'),
+    path('festivals/create/', views.FestivalCreate.as_view(), name='festivals_create'),
+    path('festivals/<int:pk>/update/', views.FestivalUpdate.as_view(), name='festivals_update'),
+    path('festivals/<int:pk>/delete/', views.FestivalDelete.as_view(), name='festivals_delete'),
 ]
